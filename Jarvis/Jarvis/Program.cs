c7cfe5dcef386f5f28bf5dc46ef2f6acd2affae9
LightDemon <page@lightdemon.me>
2016-09-19 22:39:56 -0500

    Start of Wink home automation intergration: Test methods to get user device list and set the power status of a light

--- Jarvis/Jarvis/Program.cs
+++ Jarvis/Jarvis/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Globalization;
+using System.Net.Http;
+
 
 namespace Jarvis
 {
@@ -16,12 +18,17 @@ namespace Jarvis
         static SpeechRecognitionEngine sre;
         static bool done = false;
         static bool speechOn = true;
+        static string DeviceList;
 
 
         static void Main(string[] args)
         {
             try
             {
+
+                GetDeviceList();
+                
+
                 //Set up speech
                 ss.SetOutputToDefaultAudioDevice();
                 Console.WriteLine("\n(Speaking: Hello sir)");
@@ -45,6 +52,7 @@ namespace Jarvis
                 ch_Status.Add("status report");
                 ch_Status.Add("house status");
                 ch_Status.Add("system status");
+                ch_Status.Add("print device list");
                 GrammarBuilder gb_Info = new GrammarBuilder();
                 gb_Info.Append("Jarvis");
                 gb_Info.Append(ch_Status);
@@ -56,8 +64,8 @@ namespace Jarvis
                 ch_Light.Add("living room fan");
                 ch_Light.Add("study light");
                 Choices ch_Power = new Choices();
-                ch_Power.Add("Turn on");
-                ch_Power.Add("Turn off");
+                ch_Power.Add("turn on");
+                ch_Power.Add("turn off");
                 GrammarBuilder gb_Light = new GrammarBuilder();
                 gb_Light.Append("Jarvis");
                 gb_Light.Append(ch_Power);
@@ -139,16 +147,110 @@ namespace Jarvis
             //      " equals " + sum);w
             //}
 
-            if (txt.IndexOf("report") >= 0)
+            if (txt.IndexOf("status") >= 0 && txt.IndexOf("report") >= 0)
             {
 
                 ss.SpeakAsync("status report, temporal coordinates unknown, engines offline, main power offline, auxillary power at 35%, emergency power on standby");
 
             }
 
+            if (txt.IndexOf("turn on") >= 0 && txt.IndexOf("living room light") >= 0)
+            {
+
+                ss.SpeakAsync("Turning on the living room light");
+                SetLightState("light_bulb","27105","true" );
+
+            }
+
+            if (txt.IndexOf("turn off") >= 0 && txt.IndexOf("living room light") >= 0)
+            {
+
+                ss.SpeakAsync("Turning off the living room light");
+                SetLightState("light_bulb", "27105", "flase");
+
+            }
+
+            if (txt.IndexOf("system") >= 0 && txt.IndexOf("status") >= 0)
+            {
+               // DeviceList = null;
+                GetDeviceList();
+                ss.SpeakAsync("checking, please stand by");
+                Console.Write(DeviceList);
+                if (DeviceList != null)
+                {
+                    ss.SpeakAsync("I have a connection to the wink servers and have an updated device list");
+                }
+                else
+                {
+                    ss.SpeakAsync("I have no connection to the wink servers and have purged the device list");
+                }
+            }
+
+            if (txt.IndexOf("print device list") >= 0)
+            {
+
+                Console.Write(DeviceList);
+
+            }
 
 
         } // sre_SpeechRecognized
+
+        static async void GetDeviceList()
+        {
+            try
+            {
+
+                var baseAddress = new Uri("https://private-3c4032-winkapiv2.apiary-mock.com/");
+
+                using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+                {
+
+                    using (var response = await httpClient.GetAsync("users/me/wink_devices"))
+                    {
+
+                        string responseData = await response.Content.ReadAsStringAsync();
+                        DeviceList = responseData;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                DeviceList = null;
+
+            }
+        } // GetDeviceList
+
+
+        static async void SetLightState(string d_type, string d_id, string state )
+        {
+            try
+            {
+                var baseAddress = new Uri("https://private-3c4032-winkapiv2.apiary-mock.com/");
+
+                using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+                {
+
+
+                    using (var content = new StringContent("{  \"desired_state\": {    \"powered\": " + state + "  }}", System.Text.Encoding.Default, "application/json"))
+                    {
+                        using (var response = await httpClient.PutAsync("{" + d_type + "}/{" + d_id + "}/desired_state", content))
+                        {
+                            string responseData = await response.Content.ReadAsStringAsync();
+                            Console.Write(responseData);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        } // SetLightState
+
 
 
     } // JarvisProgram
